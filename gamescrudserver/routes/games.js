869db5960e69b67f76005575d60cae6d6e7bbaf0
Abdul Rauf <raufyy4@gmail.com>
2021-12-20 21:09:14 +0500

    deleting server files

--- gamescrudserver/routes/games.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const GameController = require("../controllers/GameControllers")
-router.get("/", GameController.getAllGames) //✅
-router.get("/:id", GameController.getGame) //✅
-router.get("/page/:page", GameController.getGamesOnPage) //✅
-router.post("/", GameController.addGame) //✅
-router.put("/:id", GameController.updateGame)//✅
-router.delete("/:id", GameController.deleteGame)//✅
-module.exports = router;
